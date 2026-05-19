52ee0c14728e93d75766527e4770d3ac352c19b1
Okubule <topepaul38@gmail.com>
2026-05-19 23:18:27 +0100

    Final fix for extract from pdf

--- core/utils.py
+++ core/utils.py
@@ -14,5 +14,10 @@ def extract_text_from_docx(file):
 
 
 def extract_text_from_pdf(file):
-    pass
+    reader = PyPDF2.PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        if page.extract_text():
+            text += page.extract_text() + "\n"
+    return text
 
